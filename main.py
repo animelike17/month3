3a506c7fca8f7522a30bbf4b9fdaa94ce55b98f0
animelike17 <110774126+animelike17@users.noreply.github.com>
2022-09-15 17:47:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,65 +2,114 @@ from aiogram import types
 from aiogram.utils import executor
 from config import bot,dp
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-@dp.message_handler(commands=['start'])
-async def command_start(message: types.Message):
-    await message.answer(f'привет {message.from_user.full_name}')
-
-@dp.message_handler(commands=['quiz'])
-async def quiz1(message:types.Message):
-    markup = InlineKeyboardMarkup()
-    button_1 = InlineKeyboardButton('NEXT', callback_data='button_1')
-    markup.add(button_1)
-    question = "Кто создал Java?"
-    answer = [
-         'James Gosling',
-         'Brad Pitt',
-         'Tatjana Bakaltschuk',
-         'Napoleon',
-         'Bob Marley',
-         'Ansu Fati'
-     ]
-    await bot.send_poll(
-        chat_id= message.chat.id,
-        question= question,
-        options=answer,
-        correct_option_id=1,
-        explanation='Так кто создал',
-        type='quiz',
-        reply_markup=markup
-    )
-
-@dp.callback_query_handler(lambda call: call.data == 'button_1')
-async def quiz_2(call: types.CallbackQuery):
-    question = "Лучший кинорежиссер"
-    answer = [
-        'James Cameron',
-        'Christopher Nolan',
-        'Quentin Tarantino',
-
-    ]
-    await bot.send_poll(
-        chat_id=call.message.chat.id,
-        question=question,
-        options=answer,
-        correct_option_id=0,
-        type='quiz'
-    )
+from handlers import admin,callback,client,extra
 
 
-@dp.message_handler(commands=['mem'])
-async def mem(message: types.Message):
-    photo = open('media/mem.jpg', 'rb')
-    await bot.send_photo(message.chat.id, photo=photo)
+admin.register_handlers_admin(dp)
+client.register_handlers_client(dp)
+callback.register_handlers_callback(dp)
+extra.register_handlers_extra(dp)
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    if message.text.isdigit():
-        await bot.send_message(message.chat.id, int(message.text) * int(message.text))
-    else:
-        await bot.send_message(message.from_user.id, message.text)
+
+
+
+
+
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from aiogram import types
+# from aiogram.utils import executor
+# from config import bot,dp
+# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# @dp.message_handler(commands=['start'])
+# async def command_start(message: types.Message):
+#     await message.answer(f'привет {message.from_user.full_name}')
+
+# @dp.message_handler(commands=['quiz'])
+# async def quiz1(message:types.Message):
+#     markup = InlineKeyboardMarkup()
+#     button_1 = InlineKeyboardButton('NEXT', callback_data='button_1')
+#     markup.add(button_1)
+#     question = "Кто создал Java?"
+#     answer = [
+#          'James Gosling',
+#          'Brad Pitt',
+#          'Tatjana Bakaltschuk',
+#          'Napoleon',
+#          'Bob Marley',
+#          'Ansu Fati'
+#      ]
+#     await bot.send_poll(
+#         chat_id= message.chat.id,
+#         question= question,
+#         options=answer,
+#         correct_option_id=1,
+#         explanation='Так кто создал',
+#         type='quiz',
+#         reply_markup=markup
+#     )
+
+# @dp.callback_query_handler(lambda call: call.data == 'button_1')
+# async def quiz_2(call: types.CallbackQuery):
+#     question = "Лучший кинорежиссер"
+#     answer = [
+#         'James Cameron',
+#         'Christopher Nolan',
+#         'Quentin Tarantino',
+
+#     ]
+#     await bot.send_poll(
+#         chat_id=call.message.chat.id,
+#         question=question,
+#         options=answer,
+#         correct_option_id=0,
+#         type='quiz'
+#     )
+
+
+# @dp.message_handler(commands=['mem'])
+# async def mem(message: types.Message):
+#     photo = open('media/mem.jpg', 'rb')
+#     await bot.send_photo(message.chat.id, photo=photo)
+
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     if message.text.isdigit():
+#         await bot.send_message(message.chat.id, int(message.text) * int(message.text))
+#     else:
+#         await bot.send_message(message.from_user.id, message.text)
+
+# if __name__ == "__main__":
+#     executor.start_polling(dp, skip_updates=True)
 
